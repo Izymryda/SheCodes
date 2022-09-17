b5888e9d8988b8cb2feadfc9a8df68216263f9ba
Izymryda <witebear200396@gmail.com>
2022-09-17 14:48:08 +0300

    show the weather

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
-        <Weather />
+        <Weather city="Tokyo" />
       </header>
     </div>
   );
--- weather-react/src/Weather.js
+++ weather-react/src/Weather.js
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
 
-export default function Weather() {
+export default function Weather(props) {
+  let apiKey = "3604a3b2990de7c5ad72879ebff3eb17";
+  let url_city = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat={position.coords.latitude}&lon={position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(url_city).then(handleResponse);
+
+  function handleResponse(response) {
+    alert(`The ewather in ${props.city} is ${response.data.main.temp}°С`);
+  }
   return (
     <div>
-      <h2>Hello from Weather!!!</h2>
+      <Puff height="80" width="80" color="#4fa94d" ariaLabel="puff-loading" />
     </div>
   );
 }
