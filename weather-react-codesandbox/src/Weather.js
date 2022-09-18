1d4df321e5b5860dd3d9f0acfc2cca646c471939
Izymryda <witebear200396@gmail.com>
2022-09-19 00:09:43 +0300

    import from sandbox

---
+++ weather-react-codesandbox/src/Weather.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+
+export default function Weather(props) {
+  let [city, setCity] = useState("");
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState("10d");
+  let icon_src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+  let apiKey = "3604a3b2990de7c5ad72879ebff3eb17";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showTemperature);
+
+  function showTemperature(responce) {
+    console.log(responce.data);
+    setTemperature(Math.round(responce.data.main.temp));
+    setHumidity(responce.data.main.humidity);
+    setDescription(responce.data.weather[0].description);
+    setWind(responce.data.wind.speed);
+    setIcon(responce.data.weather[0].icon);
+  }
+
+  function updateCity(event) {
+    let search = document.querySelector(".search");
+    setCity(search.value);
+    event.preventDefault();
+    showCurrent();
+  }
+  function showCurrent() {
+    let ul = document.querySelector("ul");
+    ul.classList.add("show-ul");
+  }
+
+  return (
+    <div>
+      <form>
+        <input type="search" className="search" placeholder="Enter a city.." />
+        <input type="submit" value="Search" onClick={updateCity} />
+      </form>
+
+      <ul>
+        <li>Temperature: {temperature}°C</li>
+        <li>Description: {description}</li>
+        <li>Humidity: {humidity}%</li>
+        <li>Wind: {wind}km/h</li>
+        <li>
+          <img src={icon_src} alt="" />
+        </li>
+      </ul>
+    </div>
+  );
+}
