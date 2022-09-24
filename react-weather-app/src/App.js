e495b297eb52af744d1d5d99427a642571e6ca51
Izymryda <witebear200396@gmail.com>
2022-09-24 14:54:11 +0300

    added GitHub repo link

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Tania Slotvinska and is open-sourced on{" "}
+        <a
+          href="https://github.com/Izymryda/SheCodes/tree/master/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
