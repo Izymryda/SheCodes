31e11a43c72ea17490fd040b823ac033b20b4671
Izymryda <witebear200396@gmail.com>
2022-09-28 13:17:46 +0300

    add a URL to my cite

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -3,16 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Tania Slotvinska and is open-sourced on{" "}
-        <a
-          href="https://github.com/Izymryda/SheCodes/tree/master/react-weather-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://enchanting-mooncake-3056db.netlify.app/"
+            target="_blank"
+          >
+            Tania Slotvinska
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Izymryda/SheCodes/tree/master/react-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
