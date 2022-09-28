04289e8a1560e379994e4352c003e878af7429cd
Izymryda <witebear200396@gmail.com>
2022-09-28 21:35:05 +0300

    correct url

--- react-weather-app/src/Footer.js
+++ react-weather-app/src/Footer.js
@@ -5,13 +5,18 @@ export default function Footer() {
   return (
     <footer className="Footer">
       This project was coded by{" "}
-      <a href="https://enchanting-mooncake-3056db.netlify.app/" target="_blank">
+      <a
+        href="https://enchanting-mooncake-3056db.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Tania Slotvinska
       </a>{" "}
       and is open-sourced on{" "}
       <a
         href="https://github.com/Izymryda/SheCodes/tree/master/react-weather-app"
         target="_blank"
+        rel="noopener noreferrer"
       >
         GitHub
       </a>
